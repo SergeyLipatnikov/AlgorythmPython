b13bed931bd1d6454b3e8afbce7e5b7aac9d621d
SergeyLipatnikov <lipat995@gmail.com>
2021-05-18 22:23:09 +0300

    add odometer task

---
+++ Odometer.py
@@ -0,0 +1,25 @@
+def odometer ( oksana ):
+
+    distance = 0
+
+    for i in range(len(oksana)):
+
+        if (i % 2 == 0):
+
+            speed = oksana[i]
+
+        else:
+
+            if (i == 1):
+
+                time = oksana[i]
+
+            else:
+
+                time = oksana[i] - oksana[i-2]
+            
+            result = speed * time
+
+            distance += result
+
+    return distance
