68357dd5362cd2ef3276e648c003af3fdbacc915
salip <lipat995@gmail.com>
2021-05-24 19:04:34 +0300

    add PatternUnlock task

---
+++ PatternUnlock.py
@@ -0,0 +1,18 @@
+def PatternUnlock(N, hits):
+
+    layout = [[6,1,9],
+              [5,2,8],
+              [4,3,7]]
+
+    
+
+
+
+
+
+
+
+    return layout.index(N)
+
+
+print(PatternUnlock(6,[1]))
