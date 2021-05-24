26888b4ac62de99fdefe0121b9146d2494269c62
SergeyLipatnikov <lipat995@gmail.com>
2021-05-25 00:08:44 +0300

    fix PatternUnlock task

--- PatternUnlock.py
+++ PatternUnlock.py
@@ -4,15 +4,54 @@ def PatternUnlock(N, hits):
               [5,2,8],
               [4,3,7]]
 
-    
+    First = []
 
+    Second = []
 
+    length = 0.0
 
+    StrLength = ''
 
+    for i in range(3):
 
+            for j in range(3):
 
+                if layout[i][j] == hits[0]:
 
-    return layout.index(N)
+                    First.append(i)
+                    First.append(layout[i].index(hits[0]))
 
+    for k in range (1, N):
 
-print(PatternUnlock(6,[1]))
+        for i in range(3):
+
+            for j in range(3):
+
+                if layout[i][j] == hits[k]:
+                    
+                    Second.clear()
+                    Second.append(i)
+                    Second.append(layout[i].index(hits[k]))
+        
+        if First[0] == Second[0]:
+
+            length += 1
+
+        elif First[1] == Second[1]:
+
+            length += 1
+
+        else:
+
+            length += round((1+1) ** (0.5), 6)
+
+        First.clear()  
+        First = Second.copy()
+
+    temp = str(round(length,5))
+
+    temp2 = temp.replace(".", "")
+
+    StrLength = temp2.replace("0", "")
+
+    return StrLength
