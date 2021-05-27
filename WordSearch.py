da109448b6ed9daaa9bcb3cdb4b9780563a497e1
salip <lipat995@gmail.com>
2021-05-27 19:26:13 +0300

    fix WordSearch  task

--- WordSearch.py
+++ WordSearch.py
@@ -1,18 +1,24 @@
 def WordSearch(lens, s, subs):
 
     FinalString = []
+    String = list(s)
     StartStr = []
     Startsubspos = 0
     Subsstr_len = lens
+    s2 = ''
 
-    for i in range(len(s)):
-        s2 = ''
-        for i in range(Startsubspos,Subsstr_len):
+    for i in range(len(String)):
+        
+        if String[i] == ' ': #and len(s2) > 6:
+            s2.join(String[Startsubspos : i])
 
-            if s[i] != ' ' and len(s2) > 6:
-                s2.append(s[i])
+            FinalString.append(s2)
 
-        FinalString.append(s2)   
+            Startsubspos = i+1
+
+            s2 = '' 
+
+
         
     return FinalString
 
