7af1c4b5aac8bda417c27d40b8b668b797a00da3
SergeyLipatnikov <lipat995@gmail.com>
2021-06-22 20:55:37 +0300

    add MassVote task

---
+++ MassVote.py
@@ -0,0 +1,39 @@
+def MassVote(N, Votes):
+
+    result = ''
+
+    candidate = {a: Votes[a-1] for a in range(1,N+1)}
+
+    Votes.sort()
+
+    SummVotes = 0
+
+    for i in range(N):
+
+        SummVotes += Votes[i]
+
+    if Votes[N-1] == Votes[N-2]:
+
+        return "no winner"
+
+    else:
+
+        if round(Votes[N-1]/SummVotes*100, 3) > 50:
+
+            for Number, Values in candidate.items():
+
+                if Votes[N-1] == Values:
+
+                    return "majority winner " + str(Number)
+        
+        elif round(Votes[N-1]/SummVotes*100, 3) <= 50:
+
+            for Number, Values in candidate.items():
+
+                if Votes[N-1] == Values:
+
+                    return "minority winner " + str(Number)
+
+        
+
+print(MassVote(4, [1, 0, 1, 1]))
