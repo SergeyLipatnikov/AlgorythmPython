1dc35ec3d717bec30576bad6518b95e5f66e57a1
SergeyLipatnikov <lipat995@gmail.com>
2021-08-18 03:53:06 +0300

    upd MatrixTurn task

--- MatrixTurn.py
+++ MatrixTurn.py
@@ -1,70 +1,177 @@
 def MatrixTurn(Matrix,M,N,T):
 
-    count = 0
+    MatrixArray = CreateMatrixArray(Matrix)
 
-    String = ''
+    for i in range(T):
+        
+        count = 0
+        
+        SubStrings = []
 
-    String = CreateGorizontRightString(Matrix,String)
-    String = CreateVerticalDownString(Matrix,M,N,String)
-    String = CreateGorizontLeftString(Matrix,M,N,String)
-    String = CreateVerticalUpString(Matrix,M,String)
-    # while count < M // 2:
+        while count < M // 2:
 
-    #     String = ''
+            String = []
 
-    #     String = CreateGorizontLeftString(Matrix,)
+            String = CreateSubString(MatrixArray,M,N,String,count)
 
+            String = TurnString(String, M, N, count)
 
-    #     count += 1
+            SubStrings.append(String)
+
+            MatrixArray= ChangeSubString(MatrixArray, M, N, SubStrings,count)
+
+            count += 1
+
+    return ReturnArray(Matrix,MatrixArray)
+
+def TurnString(S, M, N, count):
+
+    if M == N and (M // 2 -1 ==count):
+
+        start = S[len(S)-1][len(S[len(S)-1])-1]
+
+        for i in range(len(S)-1,-1,-2):
+
+            end = S[i-2][len(S[i-2])-1]
+
+            for j in range(len(S[i])-1,-1,-1):
+
+                S[i][j] = S[i][j-1]
+
+            S[i][0]= end
+
+        S[1][0] = start
+    
+    else:
+
+        start = S[len(S)-1][len(S[len(S)-1])-1]
+
+        for i in range(len(S)-1,-1,-1):
+
+            end = S[i-1][len(S[i-1])-1]
+
+            for j in range(len(S[i])-1,-1,-1):
+
+                S[i][j] = S[i][j-1]
+
+            S[i][0]= end
+
+        S[0][0] = start
+
+    return S
+
+def CreateVerticalDownString(Array, M, N, String,count):
+
+    temp = []
+
+    for i in range(0+count,M-count):
+
+        temp.append(Array[i][N-1-count])
+
+    String.append(temp)
 
     return String
 
-def TurnString(S,N, Symbol):
+def CreateVerticalUpString(Array, M, String,count):
 
-    final = S[N]
+    temp = []
 
-    for i in range(N):
+    for i in range(M-1-count,-1+count,-1):
 
-        if i == N- 1:
+        temp.append(Array[i][0+count])
 
-            break
-
-        else:
-
-            S[i+1] = S[i]
-
-    S[0] = Symbol
-
-    return S, final
-
-def CreateVerticalDownString(Array, M, N, String):
-
-    for i in range(1,M-1):
-
-        String += Array[i][N-1]
+    String.append(temp)
 
     return String
 
-def CreateVerticalUpString(Array, M, String):
+def CreateGorizontRightString(Array,N,String,count):
 
-    for i in range(M-2,0,-1):
+    temp = []
 
-        String += Array[i][0]
+    for i in range(1+count,N-1-count):
 
-    return String
+        temp.append(Array[0+count][i])
 
-def CreateGorizontRightString(Array, String):
-
-    String += Array[0]
+    String.append(temp)
 
     return String
 
-def CreateGorizontLeftString(Array, M, N, String):
+def CreateGorizontLeftString(Array, M, N, String,count):
 
-    for i in range(N-1,-1,-1):
+    temp = []
 
-        String += Array[M-1][i]
+    for i in range(N-2-count,0+count,-1):
+
+        temp.append(Array[M-1-count][i])
+
+    String.append(temp)
 
     return String
 
-print(MatrixTurn(["123456", "234567", "345678", "456789"], 4,6, 3))
+def CreateSubString(Array, M, N, String,count):
+
+    String = CreateGorizontRightString(Array,N,String,count)
+    String = CreateVerticalDownString(Array,M,N,String,count)
+    String = CreateGorizontLeftString(Array,M,N,String,count)
+    String = CreateVerticalUpString(Array,M,String,count)
+
+    return String
+
+def CreateMatrixArray(MatrixString):
+
+    MatrixArray= []
+
+    for i in range(len(MatrixString)):
+
+        MatrixArray.append(list(MatrixString[i]))
+
+    return MatrixArray
+
+def ChangeGorizontRightString(Array,N,SubArray,count):
+
+    for i in range(1+count,N-1-count):
+
+        Array[0+count][i] = SubArray[count][0][i-1-count]
+
+    return Array
+
+def ChangeVerticalDownString(Array, M, N, SubArray,count):
+
+    for i in range(0+count,M-count):
+
+        Array[i][N-1-count] = SubArray[count][1][i-count]
+
+    return Array
+
+def ChangeGorizontLeftString(Array, M, N, SubArray,count):
+
+    for i in range(N-2-count,0+count,-1):
+
+        Array[M-1-count][i] = SubArray[count][2][N-2-count-i]
+
+    return Array
+
+def ChangeVerticalUpString(Array, M, SubArray,count):
+
+    for i in range(M-1-count,-1+count,-1):
+
+        Array[i][0+count] = SubArray[count][3][M-1-count-i]
+
+    return Array
+
+def ChangeSubString(Array, M, N, SubArray,count):
+
+    Array = ChangeGorizontRightString(Array,N,SubArray,count)
+    Array = ChangeVerticalDownString(Array,M,N,SubArray,count)
+    Array = ChangeGorizontLeftString(Array,M,N,SubArray,count)
+    Array = ChangeVerticalUpString(Array,M,SubArray,count)
+
+    return Array
+
+def ReturnArray(Array, turnArray):
+
+    for i in range(len(Array)):
+
+        Array[i]=''.join(turnArray[i])
+
+    return Array
